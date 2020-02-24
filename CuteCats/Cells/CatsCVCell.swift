1f9be5febee181cbdc9914a189e564b5599f3ef6
Sergi Rojas <sergirojasserra@gmail.com>
2020-02-24 18:26:14 +0100

    showing cats, not smooth and not correctly

---
+++ CuteCats/Cells/CatsCVCell.swift
@@ -0,0 +1,16 @@
+//
+//  CatsCVCell.swift
+//  CuteCats
+//
+//  Created by Sergi Rojas on 24/02/2020.
+//  Copyright © 2020 Sergi Rojas. All rights reserved.
+//
+
+import UIKit
+
+class CatsCVCell: UICollectionViewCell {
+    
+    @IBOutlet var catImageView: UIImageView!
+    
+    
+}
--- CuteCats/ViewController.swift
+++ CuteCats/ViewController.swift
@@ -8,16 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
     var cats: [Cat] = []
+    var dict: [IndexPath: UIImage] = [:]
+    
+    @IBOutlet var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        
         CatController.getCats { (cats) in
             self.cats = cats
+            self.collectionView.reloadData()
         }
+    }
+    
+//    - MARK: Collection view methods
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        self.cats.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell: CatsCVCell = collectionView.dequeueReusableCell(withReuseIdentifier: "CatsCell", for: indexPath) as? CatsCVCell {
+            if let image = self.dict[indexPath] {
+                cell.catImageView.image = image
+            } else {
+                cell.catImageView.downloaded(from: cats[indexPath.row].link)
+                if let image: UIImage = cell.catImageView.image {
+                    self.dict[indexPath] = image
+                }
+            }
+            return cell
+        }
+        return UICollectionViewCell()
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 200, height: 200)
     }
 }
 
